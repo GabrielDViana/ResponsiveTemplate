f0e3d4a2bc76e983a36dd3bde46ced590a15c1be
Gabriel Viana <gabrielv14@gmail.com>
2016-07-07 22:27:40 -0300

    First commit translating from Rails

---
+++ index.php
@@ -0,0 +1,232 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ta Viajando?</title>
+  	<link rel="stylesheet" media="all" href="/assets/stylesheets/bootstrap.min.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/font-awesome.min.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/home.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/main.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/preset.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/responsive.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/user.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" media="all" href="/assets/stylesheets/application.css" data-turbolinks-track="true" />
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+</head>
+<body>
+
+<nav >
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="#projeto">Projeto</a></li>
+        <li><a href="#livro">Livro</a></li>
+        <li><a href="#main"><img src="/assets/images/Logo.png" class="logo" alt=""></a></li>
+        <li><a href="#autora">Autora</a></li>
+        <li><a href="#atendimento">Contato</a></li>
+      </ul>
+    </div>
+</nav>
+
+
+<section id="main"style="z-index: -1;">
+  <div class="container">
+    <div class="row col-sm-10 col-sm-offset-1 distance">
+      <div>
+        <img src="/assets/images/Logo1.png" class="logo1" alt="">
+      </div>
+      <div class="col-sm-6">
+        <div class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <p style="margin-bottom: 2.5%;">Um e-Book interativo, divertido e inovador.</p>
+          <p> Traz o conhecimento de uma forma descontraída,</p>
+          <p> mostrando expressões em situações inusitadas
+          <p>e muitas vezes divertidas, surgidas do cotidiano </p>
+          <p>de qualquer viajante em trânsito.</p>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="what-is wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <img src="/assets/images/tablet.png" class="phone" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="border2"></div>
+
+  <section id="projeto" style="z-index: 22200;position: relative; ">
+      <div class="container">
+  <div class="row col-sm-10 col-sm-offset-1">
+    <h1><i class="fa fa-pencil-square-o icone" aria-hidden="true"></i> O Projeto</h1>
+    <svg class="svg" width="100%" height="30px" viewBox="0 75 400 50">
+      <line x1="-200" x2="620" y1="100" y2="100" stroke="#000" stroke-width="20" stroke-linecap="round" stroke-dasharray="1, 30"/>
+    </svg>
+    <div class="col-sm-6">
+      <iframe width="100%" height="450px" src="https://player.vimeo.com/video/168579924" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    </div>
+    <div class="col-sm-6 offset">
+      <div class=" wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <p>
+          Este projeto visa à produção de um Dicionário bilíngue (Português e Inglês).
+          Com o nome Tá Viajando? – 1001 Expressões do Brasil, o dicionário é composto
+          por 1001 expressões idiomáticas brasileiras. Contem algumas das expressões
+          mais conhecidas e usadas em todo o país e tem o intuito de apresentar
+          e facilitar o acesso dos turistas estrangeiros em vista ao Brasil este
+          rico e usado vocabulário.
+        </p>
+        <p>
+          Outro diferencial do projeto é a metodologia usada nas traduções das expressões.
+          Isso não será feito de uma forma convencional, seca e literal, mas sim
+          tentando sempre chegar a uma versão bem próxima do que será explicado
+          na língua original do leitor, criando um real significado que elucida
+          a questão a ser explanada.Desta forma, o livro aproximará a explicação
+          das nossas expressões aos leitores estrangeiros de uma forma fácil,
+          fazendo-os entender as expressões descontraidamente e com rapidez.
+        </p>
+      </div>
+      <div class="row" >
+        <div class="text-center col-sm-9 col-sm-offset-1 wow fadeInUp margins" data-wow-duration="1000ms" data-wow-delay="300ms">
+          <div class="col-sm-4">
+            <i class="fa fa-comments f-icons" aria-hidden="true"></i>
+            <h3>Diálogos
+              Port/Inglês</h3>
+          </div>
+          <div class="col-sm-4">
+            <i class="fa fa-hand-o-up f-icons" aria-hidden="true"></i>
+            <h3>Intuitivo
+              e Dinamico</h3>
+          </div>
+          <div class="col-sm-4">
+            <i class="fa fa-tablet f-icons" aria-hidden="true"></i>
+            <h3>Multi
+              Plataforma</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+  </section>
+  <div class="border2"></div>
+
+  <section id="autora">
+      <div class="container">
+  <div class="row col-sm-10 col-sm-offset-1">
+    <h1><i class="fa fa-user icone" aria-hidden="true"></i> A Autora</h1>
+    <svg class="svg" width="100%" height="30px" viewBox="0 75 400 50">
+      <line x1="-200" x2="620" y1="100" y2="100" stroke="#000" stroke-width="20" stroke-linecap="round" stroke-dasharray="1, 30"/>
+    </svg>
+    <div class="col-sm-6">
+      <div class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <img src="/assets/images/autora.jpg" alt="Aline Sanchotene">
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <h2>Aline Sanchotene</h2>
+        <p>
+          Formada pela Universidade de Brasília – UnB, habilitada em Publicidade
+          e Propaganda. Com experiência em Criação Publicitária, atividades de
+          Relações Públicas e Marketing Institucional. Trabalhou oito anos
+          (2003/2011) como Tenente no Serviço de Comunicação Social do Exército
+          Brasileiro.
+        </p>
+        <p>
+          Especialista em criação de campanhas institucionais, assessoria de
+          comunicação e projetos de comunicação corporativa. Atualmente atua na
+          iniciativa privada com marketing digital.
+        </p>
+      </div>
+      <div id="circles">
+        <img src="/assets/images/Circulos.png" style="size: 50px;" alt=""></div>
+    </div>
+  </div>
+</div>
+
+  </section>
+  <div class="border2"></div>
+
+  <section id="livro">
+    <div class="container">
+  <div class="row col-sm-10 col-sm-offset-1">
+    <h1><i class="fa fa-book icone" aria-hidden="true"></i> O Livro</h1>
+    <svg class="svg" width="100%" height="30px" viewBox="0 75 400 50">
+      <line x1="-200" x2="620" y1="100" y2="100" stroke="#000" stroke-width="20" stroke-linecap="round" stroke-dasharray="1, 30"/>
+    </svg>    <div class="col-sm-6">
+      <div class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <p>
+          Através de um e-Book e um Aplicativo, você conhecerá um dicionário com
+          o objetivo de levar estas 1001 expressões idiomáticas brasileiras de
+          uma forma descontraída, usando uma história protagonizada por um casal
+          em viagem ao Brasil, pois eles usam as expressões em situações inusitadas
+          e muitas vezes divertidas, surgidas do cotidiano de qualquer viajante
+          em trânsito.
+        </p>
+        <p>
+          Na história, O casal viaja por Brasília e Rio de Janeiro, passeando pelas
+          duas cidades com muito humor e em situações engraçadas, relacionando as
+          expressões aos acontecimentos e momentos vividos. Além do conhecimento
+          do vocabulário, a proposta é fazer com que os estrangeiros entendam como
+          se comunicar de forma fácil, descontraída e precisa com a população do Brasil.
+        </p>
+        <p>
+          Você poderá adquirir seu e-Book ou seu Aplicativo nas melhores App Stores.
+        </p>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <img src="/assets/images/book.png" class="book" alt="">
+    </div>
+  </div>
+</div>
+
+  </section>
+  <div class="border2"></div>
+
+  <section id="atendimento">
+    <div id="contact-us">
+      <div class="container">
+        <div class="container">
+  <div class="row col-sm-10 col-sm-offset-1">
+    <h1><i style="font-size:72px;"class="fa fa-group icone" aria-hidden="true"></i>Contato</h1>
+    <svg class="svg" width="100%" height="30px" viewBox="0 75 400 50">
+      <line x1="-200" x2="620" y1="100" y2="100" stroke="#000" stroke-width="20" stroke-linecap="round" stroke-dasharray="1, 30"/>
+    </svg>
+    <div class="col-sm-6">
+      <div class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <form action="/send_mail" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="scnEbOak2rvkRCYhrEN71KHC1We+y1Cu8CgHEkWWjtyD0LP2v3DAn4Ry3nL5D0psWcMgra29+Q1OZoFaWyel/g==" />
+          <div>
+            <i class="fa fa-group f-icons" aria-hidden="true"></i>
+            <input type="text" name="name" id="name" class="form-control form" placeholder="Nome Completo" />
+          </div>
+          <div>
+            <i class="fa fa-envelope f-icons" aria-hidden="true"></i>
+            <input type="email" name="email" id="email" class="form-control form" placeholder="E-mail" />
+          </div>
+          <div>
+            <i class="fa fa-phone f-icons" aria-hidden="true"></i>
+            <input type="text" name="phone" id="phone" class="form-control form" placeholder="Telefone" />
+          </div>
+          <div>
+            <i class="fa fa-map-marker f-icons" aria-hidden="true"></i>
+            <input type="text" name="state" id="state" class="form-control form" placeholder="Estado" />
+          </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <textarea name="comments" id="comments" class="form-control" rows="7" placeholder="Digite sua mensagem" style="height:100%;">
+</textarea>
+      </div>
+    </div>
+    <input type="submit" name="commit" value="Enviar" class="btn submit" />
+</form>  </div>
+</div>
+
+      </div>
+    </div>
+  </section>
+
+
+</body>
+</html>
